529b0224f15aed38ac433154524c0a4d8bf433be
Sidharth Shanker <sshanker220@gmail.com>
2012-12-24 20:59:08 -0800

    run viminstall

--- setup.sh
+++ setup.sh
@@ -2,7 +2,10 @@
 export CURRENT_DIR=`pwd -P`
 
 ln -s CURRENT_DIR/.vimrc ~/.vimrc
+CURRENT_DIR/viminstall
 
 # Step 2: install oh-my-zsh! 
 
-curl https://raw.github.com/squidarth/oh-my-zsh/tools/install.sh | zsh
+curl https://raw.github.com/squidarth/oh-my-zsh/master/tools/install.sh | zsh
+
+

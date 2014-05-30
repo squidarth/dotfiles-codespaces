3911a3ceed1a91fa2f0ee500138db8417ceffa77
Sid Shanker <sshanker220@gmail.com>
2014-05-30 15:57:38 -0400

    updates

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 # Step 1: symlink .vimrc to the .vimrc in this folder
 export CURRENT_DIR=`pwd -P`
 
-ln -s CURRENT_DIR/.vimrc ~/.vimrc
-CURRENT_DIR/viminstall
+git clone git@github.com:squidarth/dotfiles.git ~/.dotfiles/
+
+curl https://raw.github.com/squidarth/dotfiles/master/viminstall |bash
 
 # Step 2: install oh-my-zsh! 
 
 curl https://raw.github.com/squidarth/oh-my-zsh/master/tools/install.sh | zsh
 
-
+# Step 3 symlink zshrc
+ln -s ~/.dotfiles/.zshrc ~/.zshrc

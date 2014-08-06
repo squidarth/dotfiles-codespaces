be8e329e347bbec765d23c57daefadcf9e41c891
Sidharth Shanker <sshanker220@gmail.com>
2014-08-06 13:44:05 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,11 +3,11 @@ export CURRENT_DIR=`pwd -P`
 
 git clone git@github.com:squidarth/dotfiles.git ~/.dotfiles/
 
-curl https://raw.github.com/squidarth/dotfiles/master/viminstall |bash
+curl https://raw.githubusercontent.com/squidarth/dotfiles/master/viminstall |bash
 
 # Step 2: install oh-my-zsh! 
 
-curl https://raw.github.com/squidarth/oh-my-zsh/master/tools/install.sh | zsh
+curl https://raw.githubusercontent.com/squidarth/oh-my-zsh/master/tools/install.sh | zsh
 
 # Step 3 symlink zshrc
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
